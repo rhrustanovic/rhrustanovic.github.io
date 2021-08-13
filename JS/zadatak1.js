72fef82c6146c64999eb5c6e2937d1a1b0051580
rhrustanovic <rizah_hrustanovic@hotmail.com>
2021-08-13 23:50:49 +0200

    Dodan prvi zadatak

---
+++ JS/zadatak1.js
@@ -0,0 +1,7 @@
+1. Deklarirati 4 varijable, dodijeliti im proizvoljne numerièke vrijednosti. Pomoæu
+console.log funkcije ispišite zbroj prve i treæe varijable, ispišite zatim rezultat dijeljenja
+druge i èetvrte varijable.
+
+var a = 20, b = 15, c = 10, d = 5;
+console.log("Zbroj prve i treæe varijable iznosi:" + (a + c));
+console.log("Rezultat dijeljenja druge i èetvrte varijable iznosi:" + (b / d));
