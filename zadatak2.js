83a23167bb0d6d425d064b7bfe62889c7fd598d9
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2021-08-14 16:48:13 +0200

    zadatak 2

---
+++ zadatak2.js
@@ -0,0 +1,17 @@
+
+2. Ako su deklarirane varijable var a = 4, b = 44, c = 24, d = "44". Napišite koji je rezultat
+operacija ( true ili false ):
+
+
+a = 4, b = 44, c = 24, d = "44" 
+ console.log( (a > c) );
+
+console.log( (a > b) || (b > c) );
+ console.log( (a < b) && (a < c) );
+
+ console.log( (c > b) && (b < a) );
+ 
+console.log( !(a > b) || ( (a > c) && (c > b) ) );
+
+ console.log( !(a > b) );
+ console.log( (b === d) );
