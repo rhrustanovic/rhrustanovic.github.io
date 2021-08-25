3c6fa97bfa4b7f2f27d92843c2cefc23ed9f9a17
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2021-08-25 22:16:23 +0200

    zadatak 15

---
+++ zadatak15.js
@@ -0,0 +1,22 @@
+15. Zatražiti od korisnika da unose broj manji od 20. Zatim ispisati sve brojeve od unesenog
+broja do 1. Ako je unio veæi broj, ispisati uneseni broj.
+
+
+
+var a = parseInt(prompt("Unesite broj manji od 20"));
+var b = 0;
+
+if (a<20 && a>0) {
+
+for (i=a; i>b; i--) {
+
+console.log(i);
+
+}
+}
+
+else {
+
+console.log(a);
+
+}
