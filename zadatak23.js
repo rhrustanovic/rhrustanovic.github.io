748e806dd04c607905e8f3a723c496b4737873f5
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2021-09-05 23:01:37 +0200

    zadatak 23

---
+++ zadatak23.js
@@ -0,0 +1,21 @@
+23. Što æe se ispisati:
+
+//ispisat ce se: 3
+, 6=6
+, 6
+, 9
+, 11
+, 14
+
+var a = 3, b = 18, c = “6”;
+for( var i = a; i < b; i += 3 ){
+if( i = = c ){
+console.log( i + " = " + c );
+}
+if( b % i != = 0 ){
+console.log( i - 1 );
+}
+else{
+console.log( i );
+}
+}
