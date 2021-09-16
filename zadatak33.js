150cf14d33a34f678c6366e3568a5659f183d5dd
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2021-09-16 23:06:36 +0200

    zadatak 33

---
+++ zadatak33.js
@@ -0,0 +1,11 @@
+33. Preko petlje popunite niz od 10 èlanova, te ispišite niz.
+
+var unos;
+var niz =[];
+var brojac = 0;
+while (brojac < 10) {
+niz.push(parseInt(prompt("Unesite 10 èlanova niza:")));
+brojac++;
+
+}
+console.log(niz);
