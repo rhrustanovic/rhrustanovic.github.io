850b4ac264d5fa6c1524e91a64f6c783879b6c87
Rizah Hrustanović <76018126+rhrustanovic@users.noreply.github.com>
2021-10-06 16:23:10 +0200

    zadatak 1_2

---
+++ zadatak1_2.js
@@ -0,0 +1,21 @@
+/*
+1. Zatražiti od korisnika da unese 2 broja. Ukoliko je prvi broj veæi od drugog, pozvati
+funkciju koja æe oduzeti drugi od prvog. U suprotnom, pozvati funkciju koja æe ih
+zbrojiti. Ispisati rezultat u oba sluèaja.
+*/
+
+
+var broj1 = parseInt(prompt('Unesite prvi broj:'));
+var broj2 = parseInt(prompt('Unesite drugi broj:'));
+function zbroj(br1, br2){
+return br1 + br2;
+}
+function razlika(br1, br2){
+return br1 - br2;
+}
+if(broj1 > broj2){
+console.log('Razlika brojeva je: ' + razlika(broj1, broj2));
+}
+else{
+console.log('Zbroj brojeva je: ' + zbroj(broj1, broj2));
+}
